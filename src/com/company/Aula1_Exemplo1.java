ec43208dc2903e9437ab09093a5c7e3121db033f
Larissa Novais <larinovais54@gmail.com>
2021-08-24 14:08:22 -0300

    Primeiro commit 24/08

---
+++ src/com/company/Aula1_Exemplo1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Aula1_Exemplo1 {
+    public static void main(String[] args) {
+
+        int idadeDoUsuario = 22;
+        String nomeDoUsuario = "Larissa";
+
+        System.out.print("Olá, meu nome é ");
+        System.out.print(nomeDoUsuario);
+        System.out.print(" e minha idade é ");
+        System.out.print(idadeDoUsuario);
+        System.out.print(" anos");
+    }
+}
